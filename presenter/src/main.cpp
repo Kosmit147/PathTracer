cd0d564f82bc73a18a537353b5eef9d2d9f354b2
Wojciech Opara <wwojtek660@gmail.com>
2025-11-28 22:42:39 +0100

    Add an OpenGL debug message callback

--- presenter/src/main.cpp
+++ presenter/src/main.cpp
@@ -6,6 +6,7 @@
 #include <mdspan>
 #include <vector>
 
+#include "assert.hpp"
 #include "common.hpp"
 #include "defer.hpp"
 #include "log.hpp"
@@ -20,6 +21,27 @@ auto glfw_error_callback(int error_code, const char* description) -> void
 auto glfw_framebuffer_size_callback([[maybe_unused]] GLFWwindow* window, int width, int height) -> void
 {
     glViewport(0, 0, width, height);
+}
+
+auto gl_debug_message_callback([[maybe_unused]] GLenum source, [[maybe_unused]] GLenum type, GLuint id, GLenum severity,
+                               [[maybe_unused]] GLsizei length, const GLchar* message,
+                               [[maybe_unused]] const void* user_param) -> void
+{
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_NOTIFICATION:
+        PT_INFO("OpenGL Notification {}: {}", id, message);
+        break;
+    case GL_DEBUG_SEVERITY_LOW:
+        PT_WARN("OpenGL Warning {}: {}", id, message);
+        break;
+    case GL_DEBUG_SEVERITY_MEDIUM:
+    case GL_DEBUG_SEVERITY_HIGH:
+        PT_ERROR("OpenGL Error {}: {}", id, message);
+        break;
+    default:
+        PT_ASSERT(false);
+    }
 }
 
 constexpr u32 window_width = 640;
@@ -43,6 +65,9 @@ auto main() -> int
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    // TODO: Add a compile option to enable OpenGL debug messages instead of always enabling them.
+    glfwWindowHint(GLFW_CONTEXT_DEBUG, GLFW_TRUE);
+
     auto window = glfwCreateWindow(window_width, window_height, "Path Tracer", nullptr, nullptr);
 
     if (!window)
@@ -64,6 +89,11 @@ auto main() -> int
 
     glViewport(0, 0, window_width, window_height);
     glfwSetFramebufferSizeCallback(window, glfw_framebuffer_size_callback);
+
+    // TODO: Add a compile option to enable OpenGL debug messages instead of always enabling them.
+    glEnable(GL_DEBUG_OUTPUT);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    glDebugMessageCallback(gl_debug_message_callback, nullptr);
 
     static constexpr u32 image_width = 256;
     static constexpr u32 image_height = 256;
