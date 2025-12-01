6a4c3740efabcba628d07b903bd2276cc67627b4
Wojciech Opara <wwojtek660@gmail.com>
2025-12-01 12:08:07 +0100

    Add constants for infinity and pi

---
+++ tracer/include/tracer/numeric.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <limits>
+
+namespace tracer {
+
+inline constexpr auto infinity = std::numeric_limits<double>::infinity();
+
+} // namespace tracer
---
+++ tracer/include/tracer/trigonometric.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <numbers>
+
+namespace tracer {
+
+inline constexpr auto pi = std::numbers::pi;
+
+} // namespace tracer
